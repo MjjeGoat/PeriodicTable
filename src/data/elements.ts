ea875c1c9b367d36eca4903672bbefd9c1dd451c
matej <matejhanzlik25@gmail.com>
2026-04-06 12:32:26 +0200

    prvni tyden data

---
+++ src/data/elements.ts
@@ -0,0 +1,50 @@
+﻿import type {Element} from "./types.ts";
+
+export const elements: Element[] = [
+    {
+        atomicNumber: 1,
+        symbol: "H",
+        name: "Hydrogen",
+        atomicMass: 1.008,
+        group: 1,
+        period: 1,
+        category: "nonmetal",
+    },
+    {
+        atomicNumber: 2,
+        symbol: "He",
+        name: "Helium",
+        atomicMass: 4.0026,
+        group: 18,
+        period: 1,
+        category: "noble gas",
+    },
+    {
+        atomicNumber: 6,
+        symbol: "C",
+        name: "Carbon",
+        atomicMass: 12.011,
+        group: 14,
+        period: 2,
+        category: "nonmetal",
+    },
+    {
+        atomicNumber: 26,
+        symbol: "Fe",
+        name: "Iron",
+        atomicMass: 55.845,
+        group: 8,
+        period: 4,
+        category: "transition metal",
+    },
+    {
+        atomicNumber: 92,
+        symbol: "U",
+        name: "Uranium",
+        atomicMass: 238.0289,
+        group:  0, // pokud není přesně definovaná skupina
+    period: 7,
+    category: "actinide",
+    halfLife: "4.5 billion years",
+},
+];
---
+++ src/data/types.ts
@@ -0,0 +1,24 @@
+﻿export type Element = {
+    atomicNumber: number;
+    symbol: string;
+    name: string;
+    atomicMass: number;
+    group: number;
+    period: number;
+    category: string;
+    halfLife?: string;
+};
+
+export type ElementDetailProps = {
+    element: Element | null;
+};
+
+export type ElementCellProps = {
+    element: Element;
+    onClick: (element: Element) => void;
+};
+
+export type SearchBarProps = {
+    search: string;
+    setSearch: (value: string) => void;
+};
